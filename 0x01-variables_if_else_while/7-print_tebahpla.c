e1e43aac1de49b02f393ab16f5bb1e630d461d48
Flavie Okon <flaviepearl2018@gmail.com>
2023-02-22 01:57:46 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Print lowercase alphabet backwards followed by a newline.
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char i;
+
+for (i = 'z'; i >= 'a'; i--)
+{
+	putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
