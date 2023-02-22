6afa968e4f41b5df692d6dc6721825e94b041016
Flavie Okon <flaviepearl2018@gmail.com>
2023-02-22 10:27:00 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - write a program that print _putchar followed by a new line
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char c[] =
+"_putchar\n";
+	int i = 0;
+
+	while (c[] i = '\'0)
+{
+	_putchar(c[i]);
+	i ++
+}
+return (0);
+
+}
+
