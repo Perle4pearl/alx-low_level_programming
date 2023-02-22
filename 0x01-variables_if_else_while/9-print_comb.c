2b09a98e8ea9dd3e3f45813e6f2875f7ce1bc6fe
Flavie Okon <flaviepearl2018@gmail.com>
2023-02-22 02:09:08 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,11 @@
-include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 /**
  * main - Entry point
  * Description: Print all single integer numbers of base 10
- *		 stating from 0, followed by a new line
+ *	 stating from 0, followed by a new line
  * Return: Always 0 (Success)
  * Author: Flavie Okon
  */
