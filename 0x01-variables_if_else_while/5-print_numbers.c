c35f6c8a56e54729e7846889be68566a0e08ffa8
Flavie Okon <flaviepearl2018@gmail.com>
2023-02-21 18:19:07 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Print numbers 0 to 9 followed by a newline
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int lc;
+
+for (lc = 0; lc <= 9; lc++)
+{
+	printf("%d", lc);
+}
+putchar('\n');
+
+return (0);
+}
