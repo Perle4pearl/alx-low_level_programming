a3c9cb011ae8ffc693b7713e8ec1dbb576405d14
Flavie Okon <flaviepearl2018@gmail.com>
2023-02-22 01:54:27 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: Print numbers 0 to 9 followed by a newline
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int n;
+
+for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+}
+putchar('\n');
+return (0);
+}
